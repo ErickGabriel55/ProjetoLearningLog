e311c4f3770a1e781a364399b9443e7edc104b93
Erick Gabriel <erickgabrie56@gmail.com>
2025-03-20 22:35:52 -0300

    comentários

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'), # direciona para a função index. o name foi definido no base.html.
     path('topics/', views.topics, name='topics'),
-    path('topics/<int:topic_id>/', views.topic, name='topic')
+    path('topics/<int:topic_id>/', views.topic, name='topic') # Essa é uma url dinamica, pois recebe um parametro (id de Topic, nesse caso) vindo do models para que não precisemos criar uma rota pra cada tópico.
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -8,13 +8,13 @@ def index(request):
 
 def topics(request):
     """Mostra todos os assuntos"""
-    topics = Topic.objects.order_by('date_added') # acessando o banco de dados. Pegando o que ta la em Topic através do object ordenando pela data (Campo da Topic).
+    topics = Topic.objects.order_by('date_added') # acessando o banco de dados. Pegando o que ta la em Topic através do object ordenando pela data mais antiga para a mais recente(Campo da Topic).
     context = {'topics': topics} # Esse dicionário é necessário quando precisamos enviar dados coletados do banco (como o nosso topics acima) para uma página html.
     return render(request, 'learning_logs/topics.html', context) # o context precisa ser retornado junto com request e a página html.
 
 def topic(request, topic_id):
     """Mostra um único assunto e todas as suas entradas."""
-    topic = Topic.objects.get(id = topic_id)
-    entries = topic.entry_set.order_by('-date_added')
-    context = {'topic': topic, 'entries': entries}
-    return render(request, 'learning_logs/topic.html', context)
+    topic = Topic.objects.get(id = topic_id) # Passamos um id na url onde esse id vem para a view que vai pesquisar esse id para ver se tem algum tópico com aquele id. Se não tiver nenhum tópico não irá funcionar (faremos uma exceção para esse caso no html).
+    entries = topic.entry_set.order_by('-date_added') # o django cria um gerenciador nomeado como modelo_set (em letras minusculas). Isso acontece apenas quando criamos uma chave estrangeira no banco de dados. Em seguida isso é ordenado pela data mais recente para mais antiga por conta do "-" que colocamos. 
+    context = {'topic': topic, 'entries': entries} # Esse dicionário é necessário quando precisamos enviar dados coletados do banco (como o nosso topic e entries acima) para uma página html.
+    return render(request, 'learning_logs/topic.html', context) # o context precisa ser retornado junto com request e a página html.
